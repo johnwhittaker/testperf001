b4e8ae05dd8ec3ceea039378a33f7c9eacfccb30
johnwhittaker <40238960+johnwhittaker@users.noreply.github.com>
2020-03-10 14:38:04 +0000

    submit

--- listener001.php
+++ listener001.php
@@ -1,11 +1,19 @@
 <?php
 
+if(isset($_POST['minimum'])&&isset($_POST['average'])&&isset($_POST['median'])&&isset($_POST['ninetyth'])&&isset($_POST['ninetyth']))
+{
+	echo "Minimum: " . $_POST['minimum'];
+}
+#if()
+#{
+#}
+
 echo "listener - identify pause time";
 
 
-echo "You will need to run for this for the average sleep to become apparent";
+echo "You will need to run for this for the average sleep to become apparent, we suggest 1000 itterations";
 
-$x = rand(1,10)
+$x = rand(1,10);
 
 switch ($x) {
     case 1:
@@ -32,7 +40,7 @@ switch ($x) {
 	case 8:
         sleep(6);
         break;
-		case 9:
+	case 9:
         sleep(10);
         break;
 	case 10:
@@ -40,15 +48,33 @@ switch ($x) {
         break;
 }
 
-sleep(rand(1, 18));
+
 
 ?>
 
 <form name=details method="post" action="">
+<table>
+<tr><td>Minimum</td><td>
 <input type="text" name="minimum">
+</td></tr>
+<tr><td>Average</td><td>
 <input type="text" name="average">
+</td></tr>
+<tr><td>Median</td><td>
+<input type="text" name="median">
+</td></tr>
+<tr><td>90th %</td><td>
 <input type="text" name="ninetyth">
+</td></tr>
+<tr><td>95th %</td><td>
 <input type="text" name="ninetifith">
+</td></tr>
+<tr><td>Maximum</td><td>
 <input type="text" name="maximum">
+</td></tr>
+</table>
+
+
+
 <input type ="submit" value ="Submit">
 </form>
